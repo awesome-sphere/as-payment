8f8b9d1d054cbae8f740ad408453a7fd8d4d89de
khingxc <khingkhwanc@gmail.com>
2022-07-17 21:34:12 +0700

    feat: checking topic existence

--- kafka/init.go
+++ kafka/init.go
@@ -4,29 +4,49 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
-var kafkaLeader *kafka.Conn
+func ListTopic(connector *kafka.Conn) map[string]*TopicInterface {
+	partitions, err := connector.ReadPartitions()
+	if err != nil {
+		panic(err.Error())
+	}
+	m := make(map[string]*TopicInterface)
+	for _, p := range partitions {
+		if _, ok := m[p.Topic]; ok {
+			m[p.Topic].Partition += 1
+		} else {
+			m[p.Topic] = &TopicInterface{Partition: 1}
+		}
+	}
+	return m
+}
+
+func isTopicExist(connector *kafka.Conn, topic_name string) bool {
+	topic_map := ListTopic(connector)
+	_, ok := topic_map[topic_name]
+	return ok
+}
 
 func InitializeKafka() {
 	topic := "payment"
 
-	conn, err := kafka.Dial("tcp", "localhost:9095")
+	conn, err := kafka.Dial("tcp", "localhost:9092")
 	if err != nil {
 		panic(err.Error())
 	}
 	defer conn.Close()
 
-	topicConfigs := []kafka.TopicConfig{
-		{
-			Topic:             topic,
-			NumPartitions:     5,
-			ReplicationFactor: 1,
-		},
-	}
+	if !isTopicExist(conn, topic) {
+		topicConfigs := []kafka.TopicConfig{
+			{
+				Topic:             topic,
+				NumPartitions:     5,
+				ReplicationFactor: 1,
+			},
+		}
 
-	err = kafkaLeader.CreateTopics(topicConfigs...)
-	if err != nil {
-		panic(err.Error())
+		err := conn.CreateTopics(topicConfigs...)
+		if err != nil {
+			panic(err.Error())
+		}
 	}
-
-	kafkaLeader = conn
 }
---
+++ kafka/topic-interface.go
@@ -0,0 +1,5 @@
+package kafka
+
+type TopicInterface struct {
+	Partition int
+}
