8c721c0e845f584693f185147634b9eecd3c3f96
suhavni <suhavni@gmail.com>
2022-07-20 02:03:19 +0700

    changed default val to connect to the correct db

--- db/init.go
+++ db/init.go
@@ -16,8 +16,8 @@ func InitializeDatabase() {
 	dbUser := utils.GetenvOr("POSTGRES_USER", "pkinwza")
 	dbPassword := utils.GetenvOr("POSTGRES_PASSWORD", "securepassword")
 	dbHost := utils.GetenvOr("POSTGRES_HOST", "localhost")
-	dbPort := utils.GetenvOr("POSTGRES_PORT", "5432")
-	dbName := utils.GetenvOr("POSTGRES_DB", "as-cinema") // FIXME: change the database when we make one
+	dbPort := utils.GetenvOr("POSTGRES_PORT", "5434")
+	dbName := utils.GetenvOr("POSTGRES_DB", "as-payment")
 
 	dbURL := fmt.Sprintf(
 		"postgres://%s:%s@%s:%s/%s",
